a2942f5605b12abfab708af6e7ab5967fdcf5db4
Andrew Zhao <awz6ay@virginia.edu>
2020-09-13 05:39:16 -0400

    recreated the project a third time during an effort to debug, renamed to nutboxcivic

--- nutboxcivic/nutboxcivic/wsgi.py
+++ nutboxcivic/nutboxcivic/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for nutboxcivicconnect project.
+WSGI config for nutboxcivic project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nutboxcivicconnect.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nutboxcivic.settings')
 
 application = get_wsgi_application()
