22d2e44aaf984597d91b5a94b5e038b5fcb73082
Bryan Kim <bryankim@Bryans-MacBook-Pro.local>
2020-10-12 17:41:57 -0400

    added template model

--- nutboxcivic/models.py
+++ nutboxcivic/models.py
@@ -1,4 +1,9 @@
-#from django.db import models
+from django.db import models
+from django.utils import timezone
 
-#class UserData(models.Model):
-    #text = models.CharField(max_length=200)
+class template(models.Model):
+    title = models.CharField(max_length = 200)
+    shortDescription = models.CharField(max_length = 100)
+    contentTemp = models.TextField()
+    state = models.CharField(max_length = 50)
+    published = models.BooleanField()
