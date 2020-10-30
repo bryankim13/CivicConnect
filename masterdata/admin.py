158783739890371d3f020277329acd61f4567b83
Andrew Zhao <awz6ay@virginia.edu>
2020-10-30 17:27:38 -0400

    created the representative and issue models

--- masterdata/admin.py
+++ masterdata/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Emailtemplate
+from .models import Emailtemplate, Representative, Issue
 
 admin.site.register(Emailtemplate)
+admin.site.register(Representative)
+admin.site.register(Issue)
