3c3afc9347d20b8ce5b51b46a4554e614f19619a
Andrew Zhao <awz6ay@virginia.edu>
2020-10-31 14:31:45 -0400

    readded issue and representative model with a emailtemplate foreignkey in issue

--- masterdata/admin.py
+++ masterdata/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Emailtemplate
+from .models import Emailtemplate, Issue, Representative
 
 admin.site.register(Emailtemplate)
+admin.site.register(Issue)
+admin.site.register(Representative)
--- masterdata/models.py
+++ masterdata/models.py
@@ -10,10 +10,34 @@ class Emailtemplate(models.Model):
     shortDescription = models.CharField(max_length = 100)
     contentTemp = models.TextField()
     subject = models.CharField(max_length = 200)
-    state = models.CharField(max_length = 50)
+    state = models.CharField(max_length = 2)
     published = models.BooleanField(default=True)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     datecreated = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.title
+
+class Issue(models.Model):
+    emailtemplate = models.ForeignKey(Emailtemplate, on_delete=models.CASCADE)
+    title = models.CharField(max_length = 200)
+    content = models.TextField()
+    state = models.CharField(max_length= 2)
+    district = models.CharField(max_length= 5)
+    published = models.BooleanField(default=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    datecreated = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title    
+
+class Representative(models.Model):
+    name = models.CharField(max_length = 100)
+    state = models.CharField(max_length= 2)
+    district = models.CharField(max_length= 5)
+    party = models.CharField(max_length= 50)
+    email = models.CharField(max_length = 50)
+    def __str__(self):
+        return self.name
+    
+
+
 
