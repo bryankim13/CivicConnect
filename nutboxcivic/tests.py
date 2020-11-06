907fb74bc2894cbe0824a773e032455dc09d3eba
Penn Bauman <pennbauman@protonmail.com>
2020-11-05 21:31:50 -0500

    added tests that pages display

--- nutboxcivic/tests.py
+++ nutboxcivic/tests.py
@@ -1,9 +1,36 @@
 from django.test import TestCase
+from django.urls import reverse
 
-class TestTest(TestCase):
-    # Test tests are working
-    # DELETE LATER
-    def test_auto_pass(self):
-        self.assertIs(True, True)
-    #def test_auto_fail(self):
-        #self.assertIs(True, False)
+
+class TestPagesDisplay(TestCase):
+    # Test home page displays
+    def test_home(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertContains(response, "Making Your Voice Heard.")
+
+    # Test send page displays
+    def test_home(self):
+        url = reverse('send')
+        response = self.client.get(url)
+        self.assertContains(response, "Template Preview")
+        self.assertContains(response, "Copy text")
+
+    # Test create template page displays
+    def test_home(self):
+        url = reverse('createTemp')
+        response = self.client.get(url)
+        self.assertContains(response, "Making Your Voice Heard.")
+        self.assertContains(response, "Publish")
+
+    # Test select template page displays
+    def test_home(self):
+        url = reverse('select')
+        response = self.client.get(url)
+        self.assertContains(response, "View and Select Templates")
+
+    # Test gauth login page displays
+    def test_home(self):
+        url = reverse('gauth')
+        response = self.client.get(url)
+        self.assertContains(response, "Login with Google")
