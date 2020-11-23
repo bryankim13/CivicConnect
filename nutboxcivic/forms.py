1960119b8a72448d22f9549e24e57318c929fdf4
Andrew Zhao <awz6ay@virginia.edu>
2020-11-23 00:01:43 -0500

    changed the form labels for the createtemplate page, also switched the order of the form fields to be more logical

--- nutboxcivic/forms.py
+++ nutboxcivic/forms.py
@@ -4,13 +4,20 @@ from django.contrib.auth.models import User
 class templateForm(forms.ModelForm):
     class Meta:
         model = Emailtemplate
-        fields = ('title', 'shortDescription','contentTemp', 'subject', 'state')
+        fields = ('title', 'shortDescription','subject', 'contentTemp',  'state')
         widgets = {
             'title' : forms.TextInput(attrs = {'class': 'form-control'}),
             'shortDescription' : forms.TextInput(attrs = {'class' : 'form-control'}),
-            'contentTemp' : forms.Textarea(attrs = {'class' : 'form-control'}),
             'subject' : forms.TextInput(attrs = {'class' : 'form-control'}),
+            'contentTemp' : forms.Textarea(attrs = {'class' : 'form-control'}),
             'state' : forms.TextInput(attrs = {'class' : 'form-control'}),
+        }
+        labels = {
+            "title": "Email template title",
+            'shortDescription' : "Short description of the email template",
+            'subject' : "Subject of the email",
+            'contentTemp' : "Body of the email",
+            'state' : "Relevant State for the contents of the email template",
         }
 
 class UserForm(forms.ModelForm):
